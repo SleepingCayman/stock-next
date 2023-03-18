e21d093a91a212e499c5b83cafb19543dad85452
SleepingCayman <chenhouzhen@foxmail.com>
2023-03-18 13:51:23 +0700

    2

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function AboutPage() {
+export default function Supplierpage() {
   return (
     <>
       <Head>
-        <title>AboutPage</title>
+        <title>Supplier page</title>
       </Head>
-      <h1>AboutPage
+      <h1>Supplier page
       </h1>
       <p>
-      AboutPage HONGZHEN CHEN.
+      Supplier page
       </p>
       <Link href="/">Home</Link>
     </>
