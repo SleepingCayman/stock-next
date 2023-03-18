f48d519f85ef1fecbef9f2491129221a078323b0
SleepingCayman <chenhouzhen@foxmail.com>
2023-03-18 16:44:44 +0700

    3

---
+++ pages/sdatas/[id].jsx
@@ -0,0 +1,34 @@
+import Head from "next/head"
+import Link from "next/link"
+
+// Step 2: This component is rendered from the server (Server-Side Rendering) SSR
+export default function Sdata({ sdata }) {
+    console.log('sdata 2', sdata)
+    if (!sdata) return (
+      <div>
+        <p>Sdata not found</p>
+        <Link href="/sdatas">Back</Link>
+        </div>
+    );
+  
+    return (
+      <>
+        <Head>
+          <title>{sdata.supplier}</title>
+        </Head>
+        <h1>{sdata.supplier}</h1>
+        <p>{sdata.address}</p>
+        <p>{sdata.phone}</p>
+        <Link href="/sdatas">Back</Link>
+      </>
+    )
+  }
+  
+  // STEP 1: This function will be executed at the server before loading the page.
+  export async function getServerSideProps({ params }) {
+    console.debug('params', params)
+    const res = await fetch(`mongodb+srv://user1:qqqqq123@cluster0.gkl5jni.mongodb.net/sdatas/${params.id}`)
+    const sdata = await res.json()
+    console.debug('sdata 1', sdata)
+    return { props: { sdata } }
+  }
